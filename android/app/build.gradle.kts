52d1d030dcf86905b4469bfe48a3216792b7e71a
Yohanes L <yohanesfc@yahoo.com>
2026-05-28 11:19:45 +0700

    Add groq

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.sos_panic_button"
+    namespace = "com.yohanesfc.sos_panic_button"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -28,7 +28,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.sos_panic_button"
+        applicationId = "com.yohanesfc.sos_panic_button"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
